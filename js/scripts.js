895240b47c8279948dfe0637e5ce0f257d54eb24
wcjameson <williamjameson90@gmail.com>
2022-01-10 13:25:40 -0800

    Add reverse function
    Co-authored-by: Anastasiia Riabets <anastasiia.ria@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 function firstLast (string) {
   return (string.charAt(0) + string.charAt(string.length - 1)).toUpperCase();
 }
+
+function reverse (string) {
+  return string.split('').reverse().join('');
+}
