ca0e9a513fd68ffd1dcd04b135b68ef6a8ec274f
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-01-11 09:11:52 -0800

    add colors.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
-  $("p").click(function() {
-    $(".walrus-showing").show();
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
   });
 });
