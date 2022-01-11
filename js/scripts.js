099a80ff3433feb6430a4f7cedaeedbbb22a43f4
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-01-11 08:58:07 -0800

    add peek-a-boo html and styles css

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,5 @@
-function firstLast (string) {
-  return (string.charAt(0) + string.charAt(string.length - 1)).toUpperCase();
-}
-
-function reverse (string) {
-  return string.split('').reverse().join('');
-}
-
-function reverseFL (string) {
-  return string + reverse(firstLast(string));
-}
-
-function count (string) {
-  return string.charAt(parseInt(string.length / 2)) + reverseFL(string);
-}
-
 $(document).ready(function() {
-
-  let sentence = prompt("Enter your sentence: ");
-
-  $("#left").click(function() {
-    alert(sentence);
+  $("p").click(function() {
+    $(".walrus-showing").show();
   });
-
-  $("#right").click(function() {
-    alert(count(sentence));
-  });
-
-})
+});
