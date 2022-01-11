66a5ffd6c2b91f37653eafdcf3f3f9f59c50c884
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-01-11 10:22:10 -0800

    add talk.html an js buttons

--- js/scripts.js
+++ js/scripts.js
@@ -13,4 +13,17 @@ $(document).ready(function() {
     $("body").removeClass();
     $("body").addClass("red-background");
   });
+
+  $("button#hello").click(function() {
+    $("ul").prepend("<li>Hello!</li>");
+  });
+
+  $("button#goodbye").click(function() {
+    $("ul").prepend("<li>Goodbye!</li>");
+  });
+
+  $("button#stop").click(function() {
+    $("ul").prepend("<li>Stop copying me!</li>");
+  });
+  
 });
